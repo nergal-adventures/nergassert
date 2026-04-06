eb606d0c892b70660d60b0fea68948cd117c06f2
Roy Rodriguez <roy.rodriguez@ypsomed.com>
2026-04-06 23:43:25 +0200

    Assert str equals

--- include/nergassert.h
+++ include/nergassert.h
@@ -2,8 +2,6 @@
 
 #define NERG_ASSERT
 
-typedef struct Result test_result_t;
-
 typedef enum ResultCode {
   PASSED,
   FAILED
@@ -16,8 +14,12 @@ typedef struct Result {
 } test_result_t;
 
 
-#define assert_not_null(pointer, ...) \
-  eval_assert_not_null(pointer __VA_OPT__(,)__VA_ARGS__)
+#define assert_equals_str(str_1, str_2, ...) \
+  eval_assert_equals_str(str_1, str_2, __func__ __VA_OPT__(,)__VA_ARGS__)
 
-test_result_t eval_assert_not_null(void *pointer, ...);
+#define assert_not_null(pointer, ...) \
+  eval_assert_not_null(pointer, __func__ __VA_OPT__(,)__VA_ARGS__)
+
+test_result_t eval_assert_not_null(void *pointer, const char *func, ...);
+test_result_t eval_assert_equals_str(char *str_1, char *str_2, const char *func, ...);
 #endif // NERG_ASSERT
--- src/nergassert.c
+++ src/nergassert.c
@@ -1,6 +1,9 @@
 #include "nergassert.h"
 #include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ANSI_RESET_ALL    "\x1b[0m"
 #define ANSI_COLOR_GREEN  "\x1b[32m"
@@ -9,16 +12,62 @@
 #define STR_PASSED        ANSI_COLOR_GREEN "\xE2\x9C\x94 PASSED" ANSI_RESET_ALL
 #define STR_FAILED        ANSI_COLOR_RED "\xE2\x9C\x98 FAILED" ANSI_RESET_ALL
 
-test_result_t eval_assert_not_null(void *ptr, ...)
+test_result_t eval_assert_equals_str(
+    char *str_1, 
+    char *str_2, 
+    const char *func, 
+    ...
+    )
 {
+  va_list args;
+  va_start(args, func);
+  char *message = va_arg(args, char *);
+  va_end(args);
+
+  test_result_t *res = malloc(sizeof(test_result_t));
+  if (strlen(str_1) != strlen(str_2)) {
+      res->str = STR_FAILED;
+      res->result = FAILED;
+      printf("[%s]:\t%s\t '%s' not equals to '%s'\t%s\n", 
+          func, STR_FAILED, str_1, str_2, message);
+      return *res;
+  }
+  while (*str_1 != '\0' && *str_2 != '\0') {
+    if (*str_1 != *str_2) {
+      res->str = STR_FAILED;
+      res->result = FAILED;
+      printf("[%s]:\t%s\t%s\n", func, STR_FAILED, message);
+      return *res;
+      break;
+    }
+    str_1 ++;
+    str_2 ++;
+  }
+  res->str = STR_PASSED;
+  res->result = PASSED;
+  printf("[%s]:\t%s\n", func, STR_PASSED);
+
+  return *res;
+}
+
+test_result_t eval_assert_not_null(void *ptr, const char *func, ...)
+{
+  //Capture the message argument
+  va_list args;
+  va_start(args, func);
+  char *message = va_arg(args, char *);
+  va_end(args);
+
   test_result_t *res = malloc(sizeof(test_result_t));
   if (ptr != NULL) {
     res->str = STR_PASSED;
     res->result = PASSED;
+    printf("[%s]:\t%s\n", func, STR_PASSED);
   } else {
     res->str = STR_FAILED;
     res->result = FAILED;
-    assert(ptr);
+    printf("[%s]:\t%s\t%s\n", func, STR_FAILED, message);
+//    assert(ptr);
   }
   return *res;
 }
